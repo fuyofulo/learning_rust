18620a33cb1038d974e086e7e4a8f3a167165012
fuyofulo <pheonixdiaz625@gmail.com>
2025-09-22 00:07:13 +0530

    resource sharing across the app instance

--- actix_web/src/main.rs
+++ actix_web/src/main.rs
@@ -1,4 +1,10 @@
 use actix_web::{get, HttpResponse, Responder, HttpServer, App, post, web};
+use std::sync::Mutex;
+
+
+struct AppStateWithCounter{
+    counter: Mutex<i32>
+}
 
 struct AppState {
     app_name: String
@@ -6,7 +12,12 @@ struct AppState {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+
+    let counter = web::Data::new(AppStateWithCounter{
+        counter: Mutex::new(0)
+    });
+
+    HttpServer::new( move || {
         App::new()
             .service(hello)
             .service(echo)
@@ -17,10 +28,13 @@ async fn main() -> std::io::Result<()> {
                     .route("app2", web::get().to(app2))
             )
             .app_data(web::Data::new(AppState{
-                app_nam: String::from("Actix Web")
+                app_name: String::from("Actix Web")
             }))
             .service(index)
-
+            .app_data(counter.clone())
+            .service(increase)
+            .service(decrease)
+            .service(getcounter)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
@@ -51,7 +65,29 @@ async fn app2() -> impl Responder {
     HttpResponse::Ok().body("app2")
 }
 
+#[get("/index")]
 async fn index(data: web::Data<AppState>) -> impl Responder {
     let app_name = &data.app_name;
     format!("Hello {app_name}!")
 }
+
+#[post("/increase")]
+async fn increase(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
+    format!("request number: {counter}")
+}
+
+#[post("/decrease")]
+async fn decrease(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap();
+    *counter -= 1;
+    format!("request number: {counter}")
+}
+
+#[get("/getcounter")]
+async fn getcounter(data: web::Data<AppStateWithCounter>) -> String {
+    let counter = data.counter.lock().unwrap();
+    format!("counter number: {counter}")
+}
+
