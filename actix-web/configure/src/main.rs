fa1725a99846558c8ba2e5aae3ae220889434dcb
fuyofulo <pheonixdiaz625@gmail.com>
2025-09-24 18:18:52 +0530

    configure

---
+++ actix-web/configure/src/main.rs
@@ -0,0 +1,33 @@
+use actix_web::{web, HttpResponse, HttpServer, App};
+
+fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/app")
+            .route(web::get().to(|| async { HttpResponse::Ok().body("app") }))
+            .route(web::head().to(HttpResponse::MethodNotAllowed)),
+    );
+}
+
+fn scoped_config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/test")
+            .route(web::get().to(|| async { HttpResponse::Ok().body("test") }))
+            .route(web::head().to(HttpResponse::MethodNotAllowed)),
+    );
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .configure(config)
+            .service(web::scope("/api").configure(scoped_config))
+            .route(
+                "/",
+                web::get().to(|| async { HttpResponse::Ok().body("/") })
+            )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
